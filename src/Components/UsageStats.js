82d4a3538d5729e86eca22362400b2a03f400bb4
arianna choza <unachoza@gmail.com>
2020-01-25 00:29:18 -0500

    fixed request url to heroku

--- src/Components/UsageStats.js
+++ src/Components/UsageStats.js
@@ -1,38 +1,39 @@
-import React, {Component} from 'react'
-import '../App.css'
-import axios from 'axios'
+import React, { Component } from 'react';
+import '../App.css';
+import axios from 'axios';
 
-class UsageStats extends Component{
-    constructor(){
-        super()
-        this.state ={
-            userCount: 0, 
-            deletedSongsCount: 0
-        }
-    }
-componentDidMount(){
-this.userCounter()
-this.songsRemovedCounter()
+class UsageStats extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userCount: 0,
+      deletedSongsCount: 0,
+    };
+  }
+  componentDidMount() {
+    this.userCounter();
+    this.songsRemovedCounter();
+  }
+
+  userCounter = async () => {
+    const res = await axios.get('https://spotify-cancel.herokuapp.com/db/users/unique');
+    this.setState({ userCount: res.data.data[0].count });
+  };
+
+  songsRemovedCounter = async () => {
+    const res = await axios.get('https://spotify-cancel.herokuapp.com/db/deletedsongs');
+    this.setState({ deletedSongsCount: res.data.data[0].count });
+  };
+  render() {
+    const { userCount, deletedSongsCount } = this.state;
+    return (
+      <div style={{ display: 'flex', marginTop: 60 }}>
+        <p className="usage">
+          {userCount} People have used this App
+          <span style={{ marginLeft: '35px' }}>{deletedSongsCount} Songs have been removed</span>
+        </p>
+      </div>
+    );
+  }
 }
-
-   userCounter = async ()=> {
-           const res = await axios.get('postgresql-closed-20170/db/users/unique')
-            this.setState({userCount: res.data.data[0].count})
-    }
-
-    songsRemovedCounter = async () => {
-      const res = await axios.get('postgresql-closed-20170/db/deletedsongs')
-            this.setState({deletedSongsCount: res.data.data[0].count})
-
-    }
-    render() {
-        const {userCount, deletedSongsCount} = this.state        
-        return (
-            <div style={{display : "flex", marginTop: 60}}>
-                <p className="usage">{userCount} People have used this App<span style={{marginLeft: "35px"}}>{deletedSongsCount} Songs have been removed</span></p>
-            </div>
-        )
-    }
-
-}
-export default UsageStats
+export default UsageStats;
